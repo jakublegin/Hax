cc030872e610b97ff84c1e3e1b11782654857641
jakublegin <51128090+jakublegin@users.noreply.github.com>
2023-10-04 18:04:56 +0200

    Add files via upload

--- haxballroom.js
+++ haxballroom.js
@@ -1,17 +1,19 @@
+// room.js
+
 const HaxballJS = require("haxball.js");
 
 HaxballJS.then((HBInit) => {
   // Same as in Haxball Headless Host Documentation
   const room = HBInit({
-    roomName: "1v1",
+    roomName: "Haxball Versatility League",
     maxPlayers: 16,
     public: true,
     noPlayer: true,
-    token: "", // Required
+    token: "thr1.AAAAAGUdjHxCucxxAToqvQ.Dxsx9RaJoCw", // Required
   });
 
   room.setDefaultStadium("Classic");
-  room.setScoreLimit(5);
+  room.setScoreLimit(3);
   room.setTimeLimit(0);
 
   room.onRoomLink = function (link) {
